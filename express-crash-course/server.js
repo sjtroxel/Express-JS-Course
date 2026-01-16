d0e29583921c1536306ff6c912e2a43cfcfe443d
sjtroxel <sjtroxel@protonmail.com>
2026-01-16 17:16:19 -0600

    query strings & setting status codes (thru 37:35)

--- express-crash-course/server.js
+++ express-crash-course/server.js
@@ -15,13 +15,24 @@ let posts = [
 
 // get all posts
 app.get('/api/posts', (req, res) => {
-    res.json(posts);
-})
+    const limit = parseInt(req.query.limit);
+
+    if(!isNaN(limit) && limit > 0) {
+        return res.status(200).json(posts.slice(0, limit));
+    } 
+    res.status(200).json(posts);
+});
 
 // get a single post
 app.get('/api/posts/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    res.json(posts.filter((post) => post.id === id));
-})
+    const post = posts.find((post) => post.id === id);
+    // res.status(200).json(posts.filter((post) => post.id === id));
+    if (!post) {
+        res.status(404).json({ msg: `A post with the id of ${id} was not found!` });
+    } else {
+        res.status(200).json(post);
+    }
+});
 
 app.listen(port, () => console.log(`Server is running on Port ${port}!`));
