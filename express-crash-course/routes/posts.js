d9b22f31dfb58d848c3945ad61aaa77c774a0b8e
sjtroxel <sjtroxel@protonmail.com>
2026-01-18 16:19:16 -0600

    use middleware for custom- and catch-all- error-handling (thru 1:10.38)

--- express-crash-course/routes/posts.js
+++ express-crash-course/routes/posts.js
@@ -7,8 +7,9 @@ let posts = [
     {id: 3, title: 'Post Three!'}
 ];
 
+
 // get all posts
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     const limit = parseInt(req.query.limit);
     if(!isNaN(limit) && limit > 0) {
         return res.status(200).json(posts.slice(0, limit));
@@ -17,24 +18,28 @@ router.get('/', (req, res) => {
 });
 
 // get a single post
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
     if (!post) {
-        return res.status(404).json({ msg: `A post with the id of ${id} was not found!` });
+       const error = new Error(`A post with the id of ${id} was not found!`);
+       error.status = 404;
+       return next(error);
     } 
     res.status(200).json(post);
 });
 
 // create new post
-router.post('/', (req, res) => { 
+router.post('/', (req, res, next) => { 
     const newPost = {
         id: posts.length + 1,
         title: req.body.title
     };
 
     if (!newPost.title) {
-        return res.status(400).json({ msg: 'Please include a title!' });
+    const error = new Error(`Please include a title!`);
+       error.status = 400;
+       return next(error);
     }
 
     posts.push(newPost);
@@ -42,24 +47,28 @@ router.post('/', (req, res) => {
 });
 
 // update post
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
 
    if (!post) {
-        return res.status(404).json({ msg: `A post with the id of ${id} was not found!` });
+       const error = new Error(`A post with the id of ${id} was not found!`);
+       error.status = 404;
+       return next(error);
     } 
     post.title = req.body.title;
     res.status(200).json(posts);
 });
 
 // delete post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
 
    if (!post) {
-        return res.status(404).json({ msg: `A post with the id of ${id} was not found!` });
+       const error = new Error(`A post with the id of ${id} was not found!`);
+       error.status = 404;
+       return next(error);
     } 
     posts = posts.filter((post) => post.id !== id);
     res.status(200).json(posts);
--- express-crash-course/server.js
+++ express-crash-course/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
 import posts from './routes/posts.js';
+import logger from './middleware/logger.js';
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -9,10 +12,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// logger middleware
+app.use(logger);
+
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/api/posts', posts);
+
+// error handler
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on Port ${port}!`));
