d7f7b1a8f21ef03478741ddba32e71d82b67ffd9
sjtroxel <sjtroxel@protonmail.com>
2026-01-19 10:21:55 -0600

    add colors pkg for different colors of HTTP req in console (thru 1:14.18)

--- express-crash-course/middleware/error.js
+++ express-crash-course/middleware/error.js
@@ -1,10 +1,9 @@
 const errorHandler = (err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).json({ msg: err.message });
-     } else
-        res.status(500).json({ msg: err.message });
-
-    res.status(404).json({ msg: err.message });
+  if (err.status) {
+    res.status(err.status).json({ msg: err.message });
+  } else {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 export default errorHandler;
--- express-crash-course/middleware/logger.js
+++ express-crash-course/middleware/logger.js
@@ -1,6 +1,21 @@
-const logger = (req, res, next) => { 
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    next();
-}
+import colors from 'colors';
+
+const logger = (req, res, next) => {
+  const methodColors = {
+    GET: 'green',
+    POST: 'blue',
+    PUT: 'yellow',
+    DELETE: 'red',
+  };
+
+  const color = methodColors[req.method] || white;
+
+  console.log(
+    `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`[
+      color
+    ]
+  );
+  next();
+};
 
 export default logger;
