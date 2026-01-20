d6b733aca586f1dbafbd011be96e8082fc849092
sjtroxel <sjtroxel@protonmail.com>
2026-01-20 15:10:30 -0600

    add a simple JS form to front-end and connect (thru 1:36.08)

--- express-crash-course/public/js/main.js
+++ express-crash-course/public/js/main.js
@@ -1,25 +1,58 @@
 const output = document.querySelector('#output');
 const button = document.querySelector('#get-posts-btn');
+const form = document.querySelector('#add-post-form');
 
 // get and show posts
 async function showPosts() {
-    try {
+  try {
     const res = await fetch('http://localhost:8000/api/posts');
-    if (!res.ok) { 
-        throw new Error('Failed to fetch posts!');
+    if (!res.ok) {
+      throw new Error('Failed to fetch posts');
     }
+
     const posts = await res.json();
     output.innerHTML = '';
 
     posts.forEach((post) => {
-        const postEl = document.createElement('div');
-        postEl.textContent = post.title;
-        output.appendChild(postEl);
-    })        
-    } catch (error) {
-      console.log('Error fetching posts: ', error);
+      const postEl = document.createElement('div');
+      postEl.textContent = post.title;
+      output.appendChild(postEl);
+    });
+  } catch (error) {
+    console.log('Error fetching posts: ', error);
+  }
+}
+
+// submit new post
+async function addPost(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  const title = formData.get('title');
+
+  try {
+    const res = await fetch('http://localhost:8000/api/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title }),
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to add post');
     }
+
+    const newPost = await res.json();
+
+    const postEl = document.createElement('div');
+    postEl.textContent = newPost.title;
+    output.appendChild(postEl);
+    showPosts();
+  } catch (error) {
+    console.error('Error adding post');
+  }
 }
 
 // event listeners
 button.addEventListener('click', showPosts);
+form.addEventListener('submit', addPost);
