dc9da9f89b5ee097ae9f994306040f2ec13aacc3
sjtroxel <sjtroxel@protonmail.com>
2026-01-20 10:28:26 -0600

    __dirname workaround (for ES modules) + make req. from FE (thru 1:30.01)

---
+++ express-crash-course/public/js/main.js
@@ -0,0 +1,25 @@
+const output = document.querySelector('#output');
+const button = document.querySelector('#get-posts-btn');
+
+// get and show posts
+async function showPosts() {
+    try {
+    const res = await fetch('http://localhost:8000/api/posts');
+    if (!res.ok) { 
+        throw new Error('Failed to fetch posts!');
+    }
+    const posts = await res.json();
+    output.innerHTML = '';
+
+    posts.forEach((post) => {
+        const postEl = document.createElement('div');
+        postEl.textContent = post.title;
+        output.appendChild(postEl);
+    })        
+    } catch (error) {
+      console.log('Error fetching posts: ', error);
+    }
+}
+
+// event listeners
+button.addEventListener('click', showPosts);
--- express-crash-course/server.js
+++ express-crash-course/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
 const port = process.env.PORT || 8000;
+
+// get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -16,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger);
 
 // setup static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/api/posts', posts);
