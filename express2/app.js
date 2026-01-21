9f9b77708237c70b602e51873ec7d15883ab5f3c
sjtroxel <sjtroxel@protonmail.com>
2026-01-21 04:42:10 -0600

    <% syntax to pass .ejs data to views, template partials (finish thru 1:46.10)

--- express2/app.js
+++ express2/app.js
@@ -7,7 +7,11 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'My Website!',
+        message: 'Hello, from EJS!',
+        people: ['John', 'Jane', 'Jack'],
+    });
 });
 
 app.listen(8000, () => console.log('Server started!'));
